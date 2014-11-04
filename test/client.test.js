e8dd3820cbe38d2b618ff25e9c1d69b50afec243
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-11-04 14:58:35 +0000

    add test to timeout

--- test/client.test.js
+++ test/client.test.js
@@ -7,7 +7,7 @@ var nock  = require('nock');
 function mockRequest(path, response, delay) {
   return nock('https://slack.com')
     .get(path)
-    .delay(delay || 0)
+    .delayConnection(delay || 0)
     .reply(200, response || {});
 }
 
@@ -23,53 +23,67 @@ function verify(body, scope, done) {
 }
 
 describe('Client', function() {
+ this.timeout(90000);
 
   var slackClient;
 
   it('initialization fails without token', function(done) {
     assert.throws(function(){ Client(); });
     done();
-  });
+   });
   
   it('initialize with a token', function(done) {
     assert.doesNotThrow(function() { 
-      slackClient = Client('MY_TOKEN'); 
+      slackClient = Client('MY_TOKEN', { timeout: 500 }); 
     });
     done();
   });
 
-  describe('endpoints', function() {
- 
-    it('usersInfo', function(done) {
-      var body = Fixture.UsersInfo;
-      var scope = mockRequest('/api/users.info?token=MY_TOKEN&user=U023BECGF', body);
-      slackClient.usersInfo({ user: 'U023BECGF' }, verify(body.user, scope, done));
+  it('fails when request is bigger than timeout', function(done){
+    var body = Fixture.UsersInfo;
+
+    scope = mockRequest('/api/users.info?token=MY_TOKEN&user=U023BECGF', body, 1500);
+
+    slackClient.usersInfo({ user: 'U023BECGF' }, function(err) {
+     assert.ok(err);
+     done();
     });
 
-    it('usersList', function(done) {
-      var body = Fixture.UsersList;
-      var scope = mockRequest('/api/users.list?token=MY_TOKEN', body);
-      slackClient.usersList(verify(body.members, scope, done));
-    });
-
-    it('channelsHistory', function(done) {
-      var body = Fixture.ChannelsHistory;
-      var scope = mockRequest('/api/channels.history?token=MY_TOKEN&channel=C999999', body);
-      slackClient.channelsHistory({ channel: 'C999999' }, verify(body.messages, scope, done));
-      
-    });
-    
-    it('channelsInfo', function(done) {
-      var body = Fixture.ChannelsInfo;
-      var scope = mockRequest('/api/channels.info?token=MY_TOKEN&channel=C999998', body);
-      slackClient.channelsInfo({ channel: 'C999998' }, verify(body.channel, scope, done));
-    });
-
-    it('channelsList', function(done) {
-      var body = Fixture.ChannelsList;
-      var scope = mockRequest('/api/channels.list?token=MY_TOKEN', body);
-      slackClient.channelsList(verify(body.channels, scope, done));
-    });
   });
+
+ describe('endpoints', function() {
+ 
+   it('usersInfo', function(done) {
+     var body = Fixture.UsersInfo;
+     var scope = mockRequest('/api/users.info?token=MY_TOKEN&user=U023BECGF', body);
+     slackClient.usersInfo({ user: 'U023BECGF' }, verify(body.user, scope, done));
+   });
+
+   it('usersList', function(done) {
+     var body = Fixture.UsersList;
+     var scope = mockRequest('/api/users.list?token=MY_TOKEN', body);
+     slackClient.usersList(verify(body.members, scope, done));
+   });
+
+   it('channelsHistory', function(done) {
+     var body = Fixture.ChannelsHistory;
+     var scope = mockRequest('/api/channels.history?token=MY_TOKEN&channel=C999999', body);
+     slackClient.channelsHistory({ channel: 'C999999' }, verify(body.messages, scope, done));
+     
+   });
+   
+   it('channelsInfo', function(done) {
+     var body = Fixture.ChannelsInfo;
+     var scope = mockRequest('/api/channels.info?token=MY_TOKEN&channel=C999998', body);
+     slackClient.channelsInfo({ channel: 'C999998' }, verify(body.channel, scope, done));
+   });
+
+   it('channelsList', function(done) {
+     var body = Fixture.ChannelsList;
+     var scope = mockRequest('/api/channels.list?token=MY_TOKEN', body);
+     slackClient.channelsList(verify(body.channels, scope, done));
+   });
+ 
+ });
 
 });
